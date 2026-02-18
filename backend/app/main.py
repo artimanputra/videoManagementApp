756db3f74d06a441b45104434129f6ff619cfe88
Arti Manputra <79042027+artimanputra@users.noreply.github.com>
2026-02-19 00:38:00 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://video-management-app-o76a.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
